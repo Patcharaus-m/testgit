55346b407076b8ec0559687275b99a3452202031
Patcharaus-m <681110371@gmail.com>
2026-01-13 18:33:17 +0700

    updated

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ app.get("/test", (req, res) => {
     res.send("Hello World!")
 })
 
+app.post("/post-method", (req, res) => {
+    res.send("สร้างละควัย")
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
